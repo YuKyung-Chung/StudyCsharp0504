9d1ecce17a055ff5b2694911e1db0526ad6f1265
admin <admin@DESKTOP-V8UOJ36>
2021-05-04 11:17:21 +0900

    클래스 만들기

---
+++ OOPSolution/ClassTestApp/Cat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTestApp
+{
+    class Cat
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Age { get; set; }
+        //public void Meow() { Console.WriteLine("{0}, 야옹!", this.Name); }
+        public void Meow() { Console.WriteLine($"{this.Name}, 야옹!"); }
+
+        public Cat() { /*Nothing*/} // 기본 생성자
+        public Cat(string name, string color, int age) // 추가 생성자
+        {
+            //Initialization 초기화
+            this.Name = name;
+            this.Color = color;
+            this.Age = age;
+        }
+    }
+}
---
+++ OOPSolution/ClassTestApp/MainApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassTestApp
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("고양이 객체 생성");
+            Cat kitty = new Cat();
+            kitty.Name = "키티";
+            kitty.Color = "하얀색";
+            kitty.Age = 3;
+            kitty.Meow();
+
+            Cat nero = new Cat
+            {
+                Name = "네로",
+                Color = "하얀색",
+                Age = 3
+            };
+            nero.Meow();
+
+            Cat mimi = new Cat("미미","노랑",3);
+            mimi.Meow();
+        }
+    }
+}
