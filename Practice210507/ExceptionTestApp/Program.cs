6e256817ce380a7647faf989542e6824d80e6abc
admin <admin@DESKTOP-V8UOJ36>
2021-05-07 12:47:53 +0900

    예외처리 연습 오전 커밋

---
+++ Practice210507/ExceptionTestApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ExceptionTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("예외발생");
+            int[] array = new int[5];
+            for (int i = 0; i <= 5; i++)
+            {
+                try
+                {
+                    array[i] = (i + 1); //1,2,3,4,5
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"문제발생. {ex.Message} 관리자에게 문의하세요.");
+                }
+            }
+            Console.WriteLine("다른 로직 수행");
+
+            int[] list = { 107, 108, 109 };
+
+            try
+            {
+
+                var result = list[1] / 0;
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(list[i]);
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine($"예외발생 : {ex.Message}");
+                // IndexOutOfRange 예외시 다른일 처리
+                Console.WriteLine("IndexOutOfRangeException 이후 처리!");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"예외발생 : {ex.Message}");
+                Console.WriteLine("DivideByZeroException 이후 처리!");
+            }
+        }
+    }
+}
---
+++ Practice210507/GenericCollectionApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCollectionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("일반화 컬렉션 테스트 ===== ");
+
+            List<int> list = new List<int>();
+            for (int i=0;i<=100;i++)
+            {
+                list.Add(i);
+            }
+            var sum = 0;
+            foreach (var item in list)
+            {
+                sum += item;
+            }
+            Console.WriteLine($"1~100의 합 = {sum}");
+            foreach (var item in list)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine();
+
+
+            ///중요!
+            Dictionary<string, int> dics = new Dictionary<string, int>();
+            dics["하나"] = 1;
+            dics["둘"] = 2;
+            dics["셋"] = 3;
+            dics["넷"] = 4;
+            dics["다섯"] = 5;
+
+            Console.WriteLine(dics["하나"]);
+            Console.WriteLine(dics["셋"]);
+            Console.WriteLine(dics["다섯"]);
+
+            foreach (var item in dics)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+        }
+    }
+}
