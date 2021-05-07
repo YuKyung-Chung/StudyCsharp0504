02a7615e20400a4a2a759dc18e4103e747cffb83
admin <admin@DESKTOP-V8UOJ36>
2021-05-07 09:51:13 +0900

    수정 커밋

--- Practice210506/IndexerTestApp/MainApp.cs
+++ Practice210506/IndexerTestApp/MainApp.cs
@@ -6,20 +6,26 @@ using System.Threading.Tasks;
 
 namespace IndexerTestApp
 {
-    class MainApp
+    class MainApp : MyList
     {
         static void Main(string[] args)
         {
             Console.WriteLine("MyList 예제");
 
             MyList list = new MyList();
-            for (int i=0; i<5;i++)
+            for (int i = 0; i < 5; i++)
             {
-                list[i] = (i + 1); //1~5
+                list[i] = (i + 1); // 1~5 this[index] set
             }
             for (int i = 0; i < list.Length; i++)
             {
-                Console.WriteLine(list[i]);
+                Console.WriteLine(list[i]); // this[index] get
+            }
+
+            Console.WriteLine("Foreach 실행====");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
             }
         }
     }
--- Practice210506/IndexerTestApp/MyList.cs
+++ Practice210506/IndexerTestApp/MyList.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Collections;
+using System.Reflection.Metadata.Ecma335;
 
 namespace IndexerTestApp
 {
-    class MyList
+    class MyList : IEnumerable, IEnumerator
     {
         
         private int[] array;
+        private int position = -1; //배열의 현재 위치값 (0부터 시작하므로 초기값 -1로 시작)
 
+        // 배열화 (배열이 아닌 MyList를 배열처럼 만들어주는 프로퍼티)
         public int this[int index] //== MyList[i] == array[i]
         {
             get 
@@ -31,9 +35,32 @@ namespace IndexerTestApp
             get { return array.Length; } // 3
         }
 
+        // IEnumerator
+        public object Current
+        {
+            get { return array[position]; } // 현재값 foreach에만 필요
+        }
+
         public MyList()
         {
             array = new int[3]; // 0, 1, 2
+        }
+
+        // IEnumerable 메소드
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+        // IEnumerator
+        public bool MoveNext()
+        {
+            position++;
+            return (position < array.Length);
+        }
+        // IEnumerator
+        public void Reset()
+        {
+            position = -1; // 초기화
         }
     }
 }
